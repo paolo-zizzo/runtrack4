45260e50c2aa7c895a0f85973d233369b8094f1f
paolo.zizzo <paolo.zizzo@laplateforme.io>
2020-05-04 12:38:18 +0200

    push

---
+++ Jour01/Job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+<link href="style1.css" rel="stylesheet">
+<link href="style2.css" media ="(min-width: 1680px) and (max-width: 1920px)" rel="stylesheet">
+<link href="style3.css" media ="(min-width: 1280px) and (max-width: 1680px )" rel="stylesheet">
+<link href="style4.css" media ="(max-width: 1280px)" rel="stylesheet">
+
+</head>
+
+<body>
+blab
+
+</body>
+
+
+
+
+</html>
---
+++ Jour01/Job02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+<link href="style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+blab
+
+</body>
+
+
+
+
+</html>
