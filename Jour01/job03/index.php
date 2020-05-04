a6268d83eafc9d6194573ce62fc663f11e188998
paolo.zizzo <paolo.zizzo@laplateforme.io>
2020-05-04 13:28:51 +0200

    job3

---
+++ Jour01/job03/index.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<main>
+	<div>
+		<section class="section">
+		</section>
+		<section class="section">
+		</section>
+	</div>
+	<div>
+		<section class="section">
+		</section>
+		<section class="section">
+		</section>
+	</div>
+</main>
+</body>
+</html>
